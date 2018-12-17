13cf91c5f1c05aaa214d2c8351f031c1c7beef91
Alex Krol <krol.oleksandr@stfalcon.com>
2018-12-17 18:28:01 +0200

    Add demo images

--- sample/src/main/java/com/stfalcon/sample/common/models/Demo.kt
+++ sample/src/main/java/com/stfalcon/sample/common/models/Demo.kt
@@ -3,6 +3,7 @@ package com.stfalcon.sample.common.models
 object Demo {
 
     private const val POSTERS_PATH = "https://raw.githubusercontent.com/stfalcon-studio/StfalconImageViewer/master/images/posters"
+    private const val MISC_PATH = "https://raw.githubusercontent.com/stfalcon-studio/StfalconImageViewer/master/images/misc"
 
     val posters = listOf(
         Poster(url = "$POSTERS_PATH/Vincent.jpg", description = "Vincent Vega is a hitman and associate of Marsellus Wallace. He had a brother named Vic Vega who was shot and killed by an undercover cop while on a job. He worked in Amsterdam for over three years and recently returned to Los Angeles, where he has been partnered with Jules Winnfield."),
@@ -16,16 +17,16 @@ object Demo {
         Poster(url = "$POSTERS_PATH/Daniel.jpg", description = "Daniel Morales - the fastest delivery man for the local pizza parlor Pizza Joe in Marseille, France. On the last day of work, he sets a new speed record, then leaves the job to pursue a new career as a taxi driver with the blessings of his boss and co-workers. Daniel's vehicle is a white 1997 Peugeot 406..."))
 
     val horizontalImages = listOf(
-        "https://previews.123rf.com/images/nataliagesto/nataliagesto1307/nataliagesto130700135/20603274-blue-water-banners-in-long-horizontal-background-with-drops.jpg",
-        "https://gemmareynolds.files.wordpress.com/2013/11/photo-1.jpg?w=487",
-        "https://previews.123rf.com/images/nataliagesto/nataliagesto1201/nataliagesto120100414/12126770-long-horizontal-background-with-light-green-banners-backgrounds-of-water-drops.jpg",
-        "https://previews.123rf.com/images/nataliagesto/nataliagesto1202/nataliagesto120200369/12427221-serie-of-violet-and-yellow-banners-with-water-splashing-on-a-long-horizontal-background.jpg"
+        "$MISC_PATH/horizontal_colorful_1.jpg",
+        "$MISC_PATH/square_colorful.jpg",
+        "$MISC_PATH/horizontal_colorful_3.jpg",
+        "$MISC_PATH/horizontal_colorful_4.jpg"
     )
 
     val verticalImages = listOf(
-        "https://wall1.androidappsapk.co/dl/9/2/f/colorful-random-triangles.jpg",
-        "https://wallpaperplay.com/walls/full/0/3/0/191172.jpg",
-        "https://wallpaper-house.com/data/out/8/wallpaper2you_238336.jpg",
-        "http://hd-wallpapersfd.info/wp-content/uploads/HTML/Nintendo-iPhone-6/Nintendo-iPhone-691.jpg"
+        "$MISC_PATH/vertical_colorful_1.jpg",
+        "$MISC_PATH/vertical_colorful_2.jpg",
+        "$MISC_PATH/vertical_colorful_3.jpg",
+        "$MISC_PATH/vertical_colorful_4.jpg"
     )
 }
